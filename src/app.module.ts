476c21dd5be80caedd4dd3ebdbdcc5721a6d6737
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2024-05-24 15:49:38 +0700

    fix: setup db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { MongooseConfigModule } from './mongoose/mongoose.module';
 import { BookModule } from './book/book.module';
 import { LogsModule } from './logs/logs.module';
-import { Book } from './book/entities/book.entity';
-import { BookController } from './book/book.controller';
-import { BookService } from './book/book.service';
 
 @Module({
   imports: [
@@ -22,16 +18,12 @@ import { BookService } from './book/book.service';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      entities: [Book],
+      autoLoadEntities: true,
       synchronize: true,
     }),
-    
-    MongooseModule.forRoot(process.env.MONGODB_URI),
     BookModule,
     LogsModule,
-  ],
-  controllers: [BookController],
-  providers: [BookService],
+    MongooseConfigModule,
+  ]
 })
-
 export class AppModule {}
